bbcc98952cd8e97cfd386ca635dbc05b612c4551
Mona Elahi <mirfane789@gmail.com>
2022-04-22 14:58:55 -0500

    Updated main.py file

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
+# import libraries
 from tkinter import *
 from tkinter.ttk import *
 
+# import time
 from time import strftime
 
+# create window and add title
 root = Tk()
 root.title('Clock')
 
+# Define function gets current time
 def time():
     string =strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000,time)
 
 
-
+# set font, bg, fg for label and pack the label.
 label = Label(root, font=('ds-digital', 80),background ='black', foreground='red')
 label.pack(anchor='center')
 time()
 
+# create a mainloop which keeps running
 mainloop()
